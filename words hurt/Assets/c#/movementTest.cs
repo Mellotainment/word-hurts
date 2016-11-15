80ee14b022f6c60306d4f722833a1dda68ad8ea9
Mellotainment <winstoncc56@gmail.com>
2016-11-15 16:02:36 -0500

    Add files via upload

---
+++ words hurt/Assets/c#/movementTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			transform.position += new Vector3 (-1, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			transform.position += new Vector3 (1, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (0,0,-1);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (0, 0, 1);
+		}
+	}
+}
---
+++ words hurt/Assets/c#/words.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class words : MonoBehaviour {
+
+
+	public AudioClip YouStink;  
+		 
+	void Start ()   
+	{
+		
+		GetComponent<AudioSource> ().clip = YouStink;
+	}        
+
+	void OnTriggerEnter ()  
+	{
+		GetComponent<AudioSource> ().Play ();
+	}
+}
